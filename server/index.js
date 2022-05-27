d4fe4b285c3a9a5601458b13b9b605cc3bd23cca
Misbah Uddin Muib <misbah15-10057@diu.edu.bd>
2022-05-27 21:49:42 +0600

    create post and store database

--- server/index.js
+++ server/index.js
@@ -2,29 +2,30 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 const CONNECTION_URL =
-	"mongodb+srv://memoriesDB:memoriesDB123@cluster0.gncei.mongodb.net/?retryWrites=true&w=majority";
+"mongodb+srv://memoriesDB:a87ROCXOFbKe4Gs7@cluster0.gncei.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
-
-// mongoose
-// 	.connect(CONNECTION_URL,('...'))
-// 	.then(() =>
-// 		app.listen(PORT, () =>
-// 			console.log(`Server Running on Port: http://localhost:${PORT}`)
-// 		)
-// 	)
-// 	.catch((error) => console.log(error.message));
 
 mongoose
 	.connect(CONNECTION_URL)
-	.then(() => {
-		console.log(`Server Running on Port: http://localhost:${PORT}`);
-	})
+	.then(() =>
+		app.listen(PORT, () =>
+			console.log(`Server Running on Port: http://localhost:${PORT}`)
+		)
+	)
 	.catch((error) => console.log(error.message));
+
+// mongoose
+// 	.connect(CONNECTION_URL)
+// 	.then(() => {
+// 		console.log(`Server Running on Port: http://localhost:${PORT}`);
+// 	})
+// 	.catch((error) => console.log(error.message));
